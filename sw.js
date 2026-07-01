2cbe18ef2583e2c2623a12531f610a221d13ea8f
growevo-ops <growevo@gmail.com>
2026-07-01 20:54:47 +0700

    fix: new Directions link, red Book buttons, real photos (E+CC separate), hero-bg

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE='trangmotorbikers-v1';
+const CACHE='trangmotorbikers-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./chat.js','./manifest.json',
   './icons/icon-192.png','./icons/icon-512.png',
-  './img/bike.jpg','./img/bikes_row.jpg',
-  './img/bike_single.jpg','./img/bike_owner.jpg'];
+  './img/hero-bg.jpg',
+  './img/bike-E-1.jpeg','./img/bike-E-2.jpeg','./img/bike-E-3.jpeg',
+  './img/bike-CC-1.jpeg','./img/bike-CC-2.jpeg','./img/bike-CC-3.jpeg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
